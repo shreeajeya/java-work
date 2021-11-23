a68c9efb4f8e6d9215cfe4e74be453bb9a264487
shreeajeya <94341817+shreeajeya@users.noreply.github.com>
2021-11-23 15:21:29 +0530

    Add files via upload

---
+++ Myproject.java
@@ -0,0 +1,13 @@
+package Mypack;
+
+public class Myproject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstNumber = 5;
+		int secondNumber = 10;
+		int sum = firstNumber+secondNumber;
+
+	}
+
+}
